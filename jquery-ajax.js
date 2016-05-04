622a44691dbb55898b4c560815ecfb3817221629
henry6666 <aguinagahenry@gmail.com>
2016-05-04 10:44:55 +0200

    Update jquery-ajax.js

--- jquery-ajax.js
+++ jquery-ajax.js
@@ -1,1 +1,15 @@
 
+//javaScript
+// Använde jQuery och Ajax
+
+$('#my-button').click(function){console.log("Knappen klickades")
+  
+  $.get("http://api.github.com/zen", function(data,textStatus, jqXHR){
+    
+    console.log(textstatus);
+    
+    $('#show-server-text').text(data);
+    
+  })
+  
+});
